7a3ec480b4fd543b26f203bfaf8a76999d12edcb
Miorey <clem.balea@gmail.com>
2020-04-09 18:16:20 +0200

    add sass base window

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,44 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+  <v-app id="app">
     <v-content>
       <HelloWorld/>
     </v-content>
+    <v-footer
+            dark
+            padless
+    >
+      <v-card
+              class="flex"
+              flat
+              tile
+      >
+        <v-card-title class="teal">
+
+          <v-btn @click="shellOpenExternal(`https://wotlk.murlocvillage.com/fr/register`)">
+            Créer un compte
+          </v-btn>
+          <strong class="subheading">Get connected with us on social networks!</strong>
+
+          <v-spacer></v-spacer>
+
+
+          <v-btn @click="shellOpenExternal(`https://wotlk.murlocvillage.com/fr/register`)">
+            Créer un compte
+          </v-btn>
+        </v-card-title>
+
+        <v-card-text class="py-2 white--text text-center">
+          <span id="powered_by">{{ new Date().getFullYear() }} - Murloc Village</span>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
+const { shell } = require('electron')
 
 export default {
   name: 'App',
@@ -56,5 +50,26 @@ export default {
   data: () => ({
     //
   }),
+
+  methods: {
+    shellOpenExternal(url) {
+      shell.openExternal(url);
+    }
+  }
 };
 </script>
+<style>
+  @font-face {
+    font-family: "LifeCraft";
+    src: url(/images/LifeCraft_Font.ttf) format("truetype");
+
+  }
+  #app {
+    height: 100%;
+    width: 100%;
+  }
+  #powered_by {
+    font-family: "LifeCraft";
+    font-size: large;
+  }
+</style>
