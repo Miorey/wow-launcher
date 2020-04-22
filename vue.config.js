6b712a3248cae899a372d20e58458b6a986c8ce3
Miorey <Clem.Balea@GMail.COM>
2020-04-22 22:36:15 +0200

    refactor

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
 module.exports = {
     "transpileDependencies": [
         `vuetify`
-    ]
+    ],
+
+    pluginOptions: {
+        i18n: {
+            locale: `en`,
+            fallbackLocale: `en`,
+            localeDir: `locales`,
+            enableInSFC: true
+        },
+        electronBuilder: {
+            builderOptions: {
+                "productName": `Tern-Subtitle File Translator`
+            },
+        }
+    },
+    
 }
