746e7f21bd514b9e925b73b6b5301763781333e7
Miorey <clem.balea@gmail.com>
2020-04-26 13:06:55 +0200

    refactoring

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ export const configData = {
     target_path: `./Data`,
     available_language: [`frFR`, `enUS`],
     default_language: `enUS`,
-    patchlist_endpoint: `http://localhost:3000/api/launcher`,
+    patchlist_endpoint: `https://wotlk.murlocvillage.com/api/launcher`,
     end_sound: `murloc.mp3`
 }
