9dafa6dad092d39469ca0a8ab0d580c0e9597c8b
Miorey <Clem.Balea@GMail.COM>
2021-04-18 13:49:14 +0200

    fix and clean

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ try {
     console.log(`__dirname`,__dirname)
     const { patchManager } = require(`./patchManager`)
     const App = require(`./App.vue`).default
-    console.log(`I TRY IT`)
     Vue.config.productionTip = false
 
     const Translator = require(`./trans/translator`)
