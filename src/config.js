29ed982ac886ce6a45e5aa2130e0a069ea5e814a
Clement Balea <clem.balea@gmail.com>
2022-02-21 18:41:24 +0100

    fix configuration

--- src/config.js
+++ src/config.js
@@ -1,9 +1,17 @@
-const { configData } = require(`../config.js`)
+const fs = require(`fs`)
+
 
 class Config {
     constructor () {
-        this.conf = configData
+        const rawdata = fs.readFileSync(`${this.rootDir}config.json`)
+        this.conf = JSON.parse(rawdata)
+    }
+    get rootDir() {
+        return (
+            process.platform === `darwin` && process.env.NODE_ENV === `production`
+        ) ? `${process.resourcesPath}/../../../` : `./`
     }
 }
+console.log(`YOLO`)
 
 export const config  = new Config()
