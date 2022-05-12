c489e11dfe810bcd4262d50d2325c591cae51d72
Clement Balea <clem.balea@gmail.com>
2022-05-12 13:52:56 +0200

    add addons

--- src/components/AddonOptions.vue
+++ src/components/AddonOptions.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
     <span class="section_title display-1 text_wow_style">{{ `lbl_addons` | trans }}</span>
+    <div v-for="item in getAddons()" :key="item.id">
+      <input
+          :disabled="patchManager.downloadInProgress"
+          v-model="selected" type="checkbox"
+          :id="item.id" :value="item.id"
+          style="margin-right: 10px;"
+      >
+      <label class="wow_text" :for="item.id">
+        {{ item.name }}
+      </label>
+    </div>
   </div>
 </template>
 
 <script>
+const { patchManager } = require(`../patchManager`);
 export default {
-    name: `AddonOptions`
+    name: `AddonOptions`,
+    data: () => ({
+        patchManager: patchManager,
+        language: patchManager.language,
+        selected: false
+    }),
+    methods: {
+        getAddons() {
+            return patchManager.patchList.addons;
+        }
+    }
 };
 </script>
 <style scoped>
