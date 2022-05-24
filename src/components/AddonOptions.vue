24887e4094664080a5c183017167fc0b0adf3d9c
Clement Balea <clement.balea-ext@getalma.eu>
2022-05-25 00:29:53 +0200

    add addons to settings file

--- src/components/AddonOptions.vue
+++ src/components/AddonOptions.vue
@@ -17,17 +17,54 @@
 
 <script>
 const { patchManager } = require(`../patchManager`);
+const storage = require(`electron-json-storage`);
+const { EventBus } = require(`../event-bus`);
 export default {
     name: `AddonOptions`,
     data: () => ({
+        selectedAddon: null,
         patchManager: patchManager,
         language: patchManager.language,
         selected: false
     }),
+    async  mounted() {
+        this.selectedAddon = await this.findSelectedAddons();
+        patchManager.selectedAddons = this.selectedAddon;
+    },
+    watch: {
+        'selectedAddon'(val) {
+            storage.set(`selectedAddon${this.language}`, { updated: (new Date()), patches: val }, (error) => {
+                if (error) throw error;
+            });
+            patchManager.selectedAddons =  (val) ? val : [];
+        }
+    },
     methods: {
         getAddons() {
             return patchManager.patchList.addons;
-        }
+        },
+
+        /**
+         *
+         * @returns {Promise<unknown>}
+         */
+        findSelectedAddons() {
+            return new Promise((resolve, reject) => {
+                storage.get(`selectedAddon${this.language}`, (error, data) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        EventBus.$emit(`event_loader_stop`,  `storage`);
+                    }
+                    if(!data.updated) {
+
+                        resolve([]);
+                    } else {
+                        resolve(data.addons);
+                    }
+                });
+            });
+        },
     }
 };
 </script>
